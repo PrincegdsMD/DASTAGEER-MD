c0be3ccb9fb0f2a483ed08f44a8119a6b1e956d6
PrincegdsMD <158026058+PrincegdsMD@users.noreply.github.com>
2024-07-25 08:54:26 +0500

    𝙳𝚂-𝙼𝙳

--- main.js
+++ main.js
@@ -203,7 +203,7 @@ if (connection === 'close') {
     	if (update.receivedPendingNotifications) {
 			const deviceName = os.hostname();
 			const message = `DS-MD Is Now Running! ☕`
-await conn.sendMessage(923092668108@s.whatsapp.net', {
+await conn.sendMessage('923092668108@s.whatsapp.net', {
 text: message })
 		}
 }
